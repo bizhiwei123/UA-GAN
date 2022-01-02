3867372520d177eaa86b2f4d4fce7eb7a4ca695a
bizhiwei123 <48247320+bizhiwei123@users.noreply.github.com>
2022-01-02 14:16:50 +0800

    Initial

--- test.py
+++ test.py
@@ -8,16 +8,13 @@ import numpy as np
 
 if __name__ == '__main__':
     opt = load_config()
-    # opt = TestOptions().parse()  # get test options
-    # hard-code some parameters for test
-    # direction = opt.name.split('_')[0]
     opt.load_size = 256
     opt.results_dir = 'new_results/'
-    opt.num_threads = 0   # test code only supports num_threads = 1
+    opt.num_threads = 0   
     opt.batch_size = 1    # test code only supports batch_size = 1
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
-    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+    opt.display_id = -1   # no visdom display
     opt.eval = False
     opt.isTrain = False
     # opt.split = 'train' if 'Train' in opt.dataroot else 'val'
